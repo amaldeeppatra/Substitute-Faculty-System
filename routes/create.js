1a5b6df89b34e7f9fa745c7ba2872d889be018fc
Amaldeep <amaldeeppatra21@gmail.com>
2024-06-08 01:02:13 +0530

    second commit

--- routes/create.js
+++ routes/create.js
@@ -7,7 +7,9 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
     const formData = req.body;
-    console.log('Received form data:', formData);
+    console.log('Received form data:', formData.schedule);          // returns object
+    // console.log('Received form data:', formData.schedule[0]);
+    // console.log('Received form data:', formData.schedule[1]);
     res.json({ message: 'Form submitted successfully!', data: formData, success: true });
 })
 
